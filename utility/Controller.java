16c3ad1758174f8f5d4a1431b77501f51f1ff73a
alex8970 <alex8970@stud.kea.dk>
2022-11-25 23:15:09 +0100

    Login almost done

--- utility/Controller.java
+++ utility/Controller.java
@@ -2,43 +2,30 @@ package utility;
 
 public class Controller {
     UI ui = new UI();
+    FileHandler fileHandler = new FileHandler();
 
     // loginstuff ----------------------------------
-    public void loginToSystem() {
-        String username = getUsername();
-        if (isPasswordCorrect(username)) {
-            System.out.println("You entered a valid username and password, you're now signed in");
-        } else
-            System.out.println("Wrong username or password");
+
+    public void login() {
+        String username = fileHandler.loadUsername(getUsername());
+        if (!username.equals("0") && isPasswordCorrect(getPassword())) {
+
+            System.out.println("You're signed in");
+        }
     }
+
 
     public String getUsername() {
         System.out.println("Please enter your username");
         return ui.readLine();
     }
 
-    public boolean isPasswordCorrect(String userName) {
-        while (true) {
-            System.out.println("Please enter your password");
-            switch (userName) {
-                case "thomas123":
-                    if (ui.readLine().equals("swimCoach123")) {
-                        System.out.println("you're");
-                        return true;
-                    }
-                    break;
-                case "marry123":
-                    if (ui.readLine().equals("swimCoach123")) {
-                        return true;
-                    }
-                    break;
-                case "Jen123":
-                    if (ui.readLine().equals("swimCoach123")) {
-                        return true;
-                    }
-                    break;
-            }
-            return false;
-        }
+    public String getPassword() {
+        System.out.println("Please enter your password");
+        return ui.readLine();
+    }
+
+    public boolean isPasswordCorrect(String password) {
+        return !fileHandler.loadPassword(password).equals("0");
     }
 }
