634442bbd995e206ad6a3c2c49a533f5454f1780
alex8970 <alex8970@stud.kea.dk>
2022-11-25 22:10:08 +0100

    Added login + help methods

--- utility/Controller.java
+++ utility/Controller.java
@@ -1,6 +1,44 @@
 package utility;
 
 public class Controller {
-	UI ui = new UI();
+    UI ui = new UI();
 
+    // loginstuff ----------------------------------
+    public void loginToSystem() {
+        String username = getUsername();
+        if (isPasswordCorrect(username)) {
+            System.out.println("You entered a valid username and password, you're now signed in");
+        }
+    }
+
+    public String getUsername() {
+        while (true) {
+            System.out.println("Please enter your username");
+
+            return scan
+        }
+    }
+
+    public boolean isPasswordCorrect(String userName) {
+        while (true) {
+            System.out.println("Please enter your password");
+            switch (userName) {
+                case "thomas123":
+                    if (scan.nextLine.eguals("swimCoach123")) {
+                        set
+                        return true;
+                    }
+                    break;
+                case "marry123":
+                    if (scan.nextLine.eguals("swimCoach123")) {
+                        return true;
+                    }
+                    break;
+                case "Jen123":
+
+                    break;
+            }
+            return false;
+        }
+    }
 }
