7c3b0f5b95d9b6202738eefb38436e624d9b3a88
Alex793x <Alex793x@stud.kea.dk>
2022-11-29 20:22:16 +0100

    added few comments

--- utility/FileHandler.java
+++ utility/FileHandler.java
@@ -19,6 +19,10 @@ public class FileHandler {
     private PrintStream appendPrintToFile;
     private Scanner readFromFile;
 
+
+    /*
+     * This method is verifying username exist within secret file
+     */
     public String checkUsername(String username) {//reads from passwd file and gets usernames
         try {
             Scanner readLoginCredentials = new Scanner(new File("files/passwd.txt"));
@@ -31,16 +35,20 @@ public class FileHandler {
                 } else {
                     readLoginCredentials.next();
                 }
-            }
+            } // End of while loop
 
         } catch (Exception e) {
             System.out.println("File doesn't exist\n");
         }
         System.out.println("Username doesn't exist\n");
         return "0";
-    }
+    } // End of method
 
-    public String checkPassword(String password) {//reads from passwd file and gets password
+
+    /*
+     * This method verifies that prompted password works with username within secret file, then returns password if match
+     */
+    public String checkPassword(String password) {
         try {
             Scanner readLoginCredentials = new Scanner(new File("files/passwd.txt"));
 
@@ -50,20 +58,24 @@ public class FileHandler {
                     readLoginCredentials.close();
                     return loadedPassword;
                 }
-            }
+            } // End of while loop
         } catch (Exception e) {
             System.out.println("File doesn't exist\n");
         }
         System.out.println("Password is incorrect  ");
         return "0";
-    }
+    } // End of method
 
+
+    /*
+     * This method writes any added member in to the fullMemberList file
+     */
     public void writeToFullMembersList(ArrayList<Member> swimmers) {
         try {
-            //printToFile = new PrintStream(memberArrayListFile);
             printToFile = new PrintStream(memberArrayListFile);
 
             for (Member swimmer : swimmers) {
+
                 if (swimmer instanceof CompetitiveSwimmer) {
                     printToFile.print("true");
                 } else {
@@ -80,13 +92,15 @@ public class FileHandler {
                 if (swimmer instanceof CompetitiveSwimmer)
                     for (SwimmingDiscipline type : ((CompetitiveSwimmer) swimmer).getSwimmingDisciplineList()) {
                         printToFile.print((";") + (type));
-                    }
+                    } // End of inner for loop
                 printToFile.println("");
-            }
+            } // End of outer for loop
         } catch (FileNotFoundException e) {
             System.out.println("Noget gik galt");
         }
-    }
+    } // End of method
+
+
     /*public void writeToResults(ArrayList<>){
         for (int i = 0; i < type.getSwimmingDisciplineResults().size(); i++) {
             try {
@@ -111,15 +125,19 @@ public class FileHandler {
     } */
 
 
+    /*
+     * This method loads the members of the fullMemberList file into an arraylist, so we can manipulate again with
+     *    member data inside the program.
+     */
     public ArrayList<Member> loadMemberList(ArrayList<Member> membersList) {
         try {
             readFromFile = new Scanner(memberArrayListFile);
             while (readFromFile.hasNextLine()) {
                 String s = readFromFile.nextLine();
                 String[] arrOfStr = s.split(";");
-
+                int id = Integer.parseInt(arrOfStr[1]);
                 if (arrOfStr[0].equalsIgnoreCase("true")) {
-                    int id = Integer.parseInt(arrOfStr[1]);
+
                     String name = arrOfStr[2];
                     String phone = arrOfStr[3];
                     String dob = arrOfStr[4];
@@ -127,16 +145,15 @@ public class FileHandler {
                     boolean hasPaid = Boolean.parseBoolean(arrOfStr[6]);
 
                     CompetitiveSwimmer compSwimmer = new CompetitiveSwimmer(id,
-                            name, phone, dob, membershipActive,hasPaid);
+                            name, phone, dob, membershipActive, hasPaid);
 
                     int disciplineAmount = arrOfStr.length - 7;
                     for (int i = 0; i < disciplineAmount; i++) {
                         compSwimmer.getSwimmingDisciplineList().add(new SwimmingDiscipline
-                                (arrOfStr[7 + i])); //magicnumber is to get the start pos enumSwimDisciplin in array
+                                (arrOfStr[7 + i])); // magicNumber is to get the start pos enumSwimDiscipline in array
                     }
                     membersList.add(compSwimmer);
                 } else {
-                    int id = Integer.parseInt(arrOfStr[1]);
                     String name = arrOfStr[2];
                     String phone = arrOfStr[3];
                     String dob = arrOfStr[4];
@@ -146,14 +163,18 @@ public class FileHandler {
                             name, phone, dob, membershipActive, hasPaid);
                     membersList.add(leisureSwimmer);
                 }
-            }
+            }   // End of while loop
             return membersList;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
+        } // End of Catch
         return membersList;
-    }
+    } // End of method
 
+
+    /*
+     * This method ensures that unique ID always continues from last MemberID within fullMemberList file
+     */
     public int loadID() {
         ArrayList<Integer> idArray = new ArrayList<>();
         try {
@@ -162,8 +183,8 @@ public class FileHandler {
                 String s = readFromFile.nextLine();
                 String[] arrOfStr = s.split(";");
                 idArray.add(Integer.parseInt(arrOfStr[1]));
-            }
-            return idArray.get(idArray.size()-1)+1;
+            } // End of while loop
+            return idArray.get(idArray.size() - 1) + 1;
         } catch (IndexOutOfBoundsException | FileNotFoundException e) {
             return 1000;
         }
